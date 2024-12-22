5ba71e1dcd170fe9ad844ccd5a0ffc221766f5a8
Rupesh Lakshman <codewithruppu@gmail.com>
2024-12-22 16:22:52 +0530

    compose file

--- docker-nginx/server.js
+++ docker-nginx/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-console.log(__dirname);
+const appName = process.env.APP_NAME;
 
 const app = express();
 const port = 3000;
@@ -10,9 +10,9 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
-	console.log("Request served by node app: index.html");
+	console.log(`Request served by node app: index.html - ${appName}`);
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port} - ${appName}`);
 });
